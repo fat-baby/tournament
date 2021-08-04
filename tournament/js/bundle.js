5afd581ccaf08ef2bed6bc15d3aebf45052cf1a9
SteamedBun <christinahuman14@gmail.com>
2021-08-04 13:49:31 +0200

    rematch

--- tournament/js/bundle.js
+++ tournament/js/bundle.js
@@ -21,7 +21,7 @@ const zeroPad = (s, i) => {
 }
 
 // Set the date we're counting down to
-var countDownDate = new Date("Aug 4, 2021 12:58:00 GMT+0200").getTime();
+var countDownDate = new Date("Aug 5, 2021 01:45:00 GMT+0200").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
