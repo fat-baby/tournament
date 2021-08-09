111fbcaacbe051cd043278742c7c3607bc4426f6
SteamedBun <christinahuman14@gmail.com>
2021-08-09 18:23:34 +0200

    set time

--- tournament/js/bundle.js
+++ tournament/js/bundle.js
@@ -21,7 +21,7 @@ const zeroPad = (s, i) => {
 }
 
 // Set the date we're counting down to
-var countDownDate = new Date("Aug 9, 2021 17:46:00 GMT+0200").getTime();
+var countDownDate = new Date("Aug 10, 2021 18:19:00 GMT+0200").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
