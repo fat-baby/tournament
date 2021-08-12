55991d037d3dcd631150b3690f29ef73c658b53b
S1NC0S <s1nc0s@protonmail.com>
2021-08-12 11:22:34 +0200

    update timer

--- tournament/js/bundle.js
+++ tournament/js/bundle.js
@@ -21,7 +21,7 @@ const zeroPad = (s, i) => {
 }
 
 // Set the date we're counting down to
-var countDownDate = new Date("Aug 11, 2021 22:05:00 GMT+0200").getTime();
+var countDownDate = new Date("Aug 12, 2021 10:17:00 GMT+0200").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -44,7 +44,8 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("timer").innerHTML = "COMING SOON";
+    document.getElementById("timer").innerHTML = "BATTLE OVER";
+    // document.getElementById("timer").innerHTML = "COMING SOON";
   }
 }, 1000);
 
